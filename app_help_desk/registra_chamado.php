096ee082c8b08db0e92368fa4533a845238b9e43
Rhanon Macley <42503409+RhMacley@users.noreply.github.com>
2020-09-29 21:53:41 -0300

    First commit

---
+++ app_help_desk/registra_chamado.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
+
+// $names = implode('#', $_POST);
+
+$texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+$arquivo = fopen('arquivo.txt', 'a');
+
+fwrite($arquivo, $texto);
+
+fclose($arquivo);
+
+header('Location: abrir_chamado.php');
